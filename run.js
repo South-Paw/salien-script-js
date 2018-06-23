6fb6f866e39c3f54599e29f1cb24d9dbd8ea7b97
Alex Gabites <alex@netspeed.net.nz>
2018-06-23 17:09:52 +1200

    Begin rewrite of the script 🎉

--- run.js
+++ run.js
@@ -5,7 +5,10 @@ const SalienScript = require('./src/index.js');
 let token;
 
 try {
-  token = fs.readFileSync('./token.txt').toString().trim();
+  token = fs
+    .readFileSync('./token.txt')
+    .toString()
+    .trim();
 } catch (e) {
   console.log("\nYou probably haven't created a token file...");
   console.log('Please read the README.md\n');
@@ -15,5 +18,5 @@ try {
 if (token) {
   const salien = new SalienScript({ token });
 
-  salien.run();
+  salien.init();
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * MIT License
  *
- * Copyright (C) 2018 Alex Gabites
+ * Copyright (c) 2018 Alex Gabites
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,275 +22,203 @@
  * SOFTWARE.
  */
 
+const dateFormat = require('dateformat');
 const delay = require('delay');
 const fetch = require('fetch-retry');
 
-const baseUrl = 'https://community.steam-api.com/';
+const { version: pkgVersion } = require('../package.json');
 
-const getUrl = (method, params = '') => `${baseUrl}/${method}${params ? '/?' : ''}${params}`;
+// eslint-disable-next-line no-console
+const logger = (...messages) => console.log(chalk.white(dateFormat(new Date(), '[HH:MM:ss]')), ...messages);
 
-const getOptions = (options = {}) => {
-  return {
-    retries: 3,
-    retryDelay: 1000,
-    headers: {
-      'Accept': '*/*',
-      'Origin': 'https://steamcommunity.com',
-      'Referer': 'https://steamcommunity.com/saliengame/play/',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
-    },
-    ...options,
-  };
-};
+// eslint-disable-next-line no-console
+const debug = message => console.log(`${JSON.stringify(message, 0, 2)}`);
 
-const getScoreForZone = (zone) => {
-  let score = 0;
-
-  switch (zone.difficulty) {
-    case 1: score = 5; break;
-    case 2: score = 10; break;
-    case 3: score = 20; break;
+class SalienScriptException {
+  constructor(message) {
+    this.name = 'SalienScriptException';
+    this.message = message;
   }
-
-  return score * 120;
-};
-
-async function getFirstAvailablePlanetId() {
-  console.log('Attempting to get first open planet...');
-
-  const request = await fetch(getUrl('ITerritoryControlMinigameService/GetPlanets/v0001', 'active_only=1'), getOptions());
-  const response = await request.json();
-
-  if (!response || !response.response.planets) {
-    console.log('Didn\'t find any planets.');
-
-    return null;
-  }
-
-  const firstOpen = response.response.planets.filter(planet => !planet.state.captured)[0];
-
-  console.log('First open planet id:', firstOpen.id);
-
-  return firstOpen.id;
-};
-
-async function getPlayerInfo(token) {
-  console.log('Getting player info...');
-
-  const request = await fetch(getUrl('ITerritoryControlMinigameService/GetPlayerInfo/v0001', `access_token=${token}`), getOptions({ method: 'POST' }));
-  const response = await request.json();
-
-  if (!response || !response.response) {
-    console.log('Didn\'t get any player info.');
-
-    return null;
-  }
-
-  console.log('Got player info!');
-
-  return response.response;
-};
-
-async function leaveCurrentGame(token, leaveCurrentPlanet) {
-  let playerInfo = null;
-
-  while (!playerInfo) {
-    playerInfo = await getPlayerInfo(token);
-  }
-
-  // Please do not change our clanid if you are going to use this script
-  // If you want to cheat for your own group, come up with up with your own approach, thank you
-  if (!playerInfo['clan_info'] || !playerInfo['clan_info']['accountid'] || playerInfo['clan_info']['accountid'] != 4777282) {
-    await fetch(getUrl('ITerritoryControlMinigameService/RepresentClan/v0001', `clanid=4777282&access_token=${token}`), getOptions({ method: 'POST' }));
-  }
-
-  while (!playerInfo) {
-    playerInfo = await getPlayerInfo(token);
-  }
-
-  if (!playerInfo['clan_info'] || !playerInfo['clan_info']['accountid'] || playerInfo['clan_info']['accountid'] != 4777282) {
-    console.log('\nYou need to join the SteamDB group to use this script!\n');
-    console.log('https://steamcommunity.com/groups/SteamDB\n');
-    process.exit(1);
-  }
-
-  if (playerInfo['active_zone_game']) {
-    console.log('Leaving `active_zone_game`...');
-
-    await fetch(getUrl('IMiniGameService/LeaveGame/v0001', `access_token=${token}&gameid=${playerInfo['active_zone_game']}`), getOptions({ method: 'POST' }));
-
-    console.log('Success!');
-  }
-
-  if (!playerInfo['active_planet']) {
-    return 0;
-  }
-
-  if (leaveCurrentPlanet) {
-    console.log('Leaving `active_planet`...');
-
-    await fetch(getUrl('IMiniGameService/LeaveGame/v0001', `access_token=${token}&gameid=${playerInfo['active_planet']}`), getOptions({ method: 'POST' }));
-
-    console.log('Success!');
-  }
-
-  return playerInfo['active_planet'];
 }
 
-async function joinPlanet(token, planetId) {
-  console.log('Attempting to join planet id:', planetId);
-
-  await fetch(getUrl('ITerritoryControlMinigameService/JoinPlanet/v0001', `id=${planetId}&access_token=${token}`), getOptions({ method: 'POST' }));
-
-  console.log('Joined!');
-
-  return;
-}
-
-async function getFirstAvailableZone(planetId) {
-  console.log(`Requesting zones for planet ${planetId}...`);
-
-  const request = await fetch(getUrl('ITerritoryControlMinigameService/GetPlanet/v0001', `id=${planetId}`), getOptions());
-  const response = await request.json();
-
-  if (!response.response.planets[0].zones) {
-    return null;
+class SalienScriptRestart {
+  constructor(message) {
+    this.name = 'SalienScriptRestart';
+    this.message = message;
   }
-
-  let zones = response.response.planets[0].zones;
-  let cleanZones = [];
-  let bossZone = null;
-
-  zones.some(zone => {
-    if (zone.captured) {
-      return;
-    }
-
-    if (zone.type === 4) {
-      bossZone = zone;
-    } else if (zone.type != 3) {
-      console.log('Unknown zone type:', zone.type);
-    }
-
-    if (zone['capture_progress'] < 0.95) {
-      cleanZones.push(zone);
-    }
-  })
-
-  if (bossZone) {
-    return bossZone;
-  }
-
-  if (cleanZones.length < 0) {
-    return null;
-  }
-
-  cleanZones.sort((a, b) => {
-    if (a.difficulty === b.difficulty) {
-      return b['zone_position'] - a['zone_position'];
-    }
-
-    return b.difficulty - a.difficulty;
-  });
-
-  return cleanZones[0];
-}
-
-async function joinZone(token, position) {
-  console.log('Attempting to join zone position:', position);
-
-  const request = await fetch(getUrl('ITerritoryControlMinigameService/JoinZone/v0001', `zone_position=${position}&access_token=${token}`), getOptions({ method: 'POST' }));
-  const response = await request.json();
-
-  if (!response || !response.response['zone_info']) {
-    console.log('Failed to join a zone.');
-
-    return null;
-  }
-
-  console.log('Got player info!');
-
-  return response.response;
-}
-
-async function reportScore(token, score) {
-  console.log('Attempting to send score...');
-
-  const request = await fetch(getUrl('ITerritoryControlMinigameService/ReportScore/v0001', `access_token=${token}&score=${score}&language=english`), getOptions({ method: 'POST' }));
-  const response = await request.json();
-
-  if (response.response['new_score']) {
-    const data = response.response;
-
-    console.log(`Score: ${data['old_score']} => ${data['new_score']} (next level: ${data['next_level_score']}) - Current level: ${data['new_level']}`);
-  }
-
-  return;
 }
 
 class SalienScript {
   constructor({ token }) {
     this.token = token;
-    this.currentPlanetId = null;
+
+    this.maxRetries = 2;
+    this.defaultDelayMs = 5000;
+    this.defaultDelaySec = this.defaultDelayMs / 1000;
   }
 
-  async run() {
-    console.log('This script will not work until you have joined our group:');
-    console.log('https://steamcommunity.com/groups/SteamDB');
+  async RequestAPI(method, params, additionalOptions = {}) {
+    let url = `https://community.steam-api.com/${method}/v0001`;
 
-    while (!this.currentPlanetId) {
-      this.currentPlanetId = await getFirstAvailablePlanetId();
+    if (params) {
+      url += '/?';
 
-      if (!this.currentPlanetId) {
-        console.log('Trying to get another PlanetId in 5 seconds...');
+      params.forEach(param => {
+        url += `${param}&`;
+      });
 
-        await delay(5000);
+      url = url.substring(0, url.length - 1);
+    }
+
+    const options = {
+      retries: 3,
+      retryDelay: 1000,
+      headers: {
+        Accept: '*/*',
+        Origin: 'https://steamcommunity.com',
+        Referer: 'https://steamcommunity.com/saliengame/play/',
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+      },
+      ...additionalOptions,
+    };
+
+    let request;
+    let response;
+    let retries = 0;
+
+    while (!response && retries < this.maxRetries) {
+      try {
+        logger(chalk.blue(`Sending ${method}...`));
+        request = await fetch(url, options);
+        response = await request.json();
+      } catch (e) {
+        logger(chalk.bgRed(`${e.name}:`), chalk.red(`For ${method}`));
+        debug(e);
+
+        retries += 1;
+
+        if (retries < this.maxRetries) {
+          logger(chalk.yellow(`Retrying ${method} in ${this.defaultDelaySec} seconds...`));
+        } else {
+          throw new SalienScriptException(`Failed ${method} after ${retries} retries`);
+        }
+
+        await delay(this.defaultDelayMs);
       }
     }
 
-    await leaveCurrentGame(this.token, true);
+    return response.response;
+  }
 
-    await joinPlanet(this.token, this.currentPlanetId);
+  async ApiGetPlanets() {
+    const response = await this.RequestAPI('ITerritoryControlMinigameService/GetPlanets', ['active_only=1']);
+    return response.planets;
+  }
 
-    this.currentPlanetId = await leaveCurrentGame(this.token, false);
+  async ApiGetPlanet(planetId) {
+    const response = await this.RequestAPI('ITerritoryControlMinigameService/GetPlanet', [`id=${planetId}`]);
+    return response;
+  }
 
-    while (true) {
-      let zone = null;
-      let joinedZone = null;
+  async ApiGetPlayerInfo() {
+    const response = await this.RequestAPI(
+      'ITerritoryControlMinigameService/GetPlayerInfo',
+      [`access_token=${this.token}`],
+      { method: 'POST' },
+    );
+    return response;
+  }
 
-      while (!zone) {
-        zone = await getFirstAvailableZone(this.currentPlanetId);
+  async ApiRepresentClan(clanId) {
+    const response = await this.RequestAPI(
+      'ITerritoryControlMinigameService/RepresentClan',
+      [`access_token=${this.token}`, `clanid=${clanId}`],
+      { method: 'POST' },
+    );
+    return response;
+  }
 
-        if (!zone) {
-          console.log('Trying to get another ZoneId in 5 seconds...');
+  async ApiLeaveGame(gameId) {
+    const response = await this.RequestAPI(
+      'IMiniGameService/LeaveGame',
+      [`access_token=${this.token}`, `gameid=${gameId}`],
+      { method: 'POST' },
+    );
+    return response;
+  }
 
-          await delay(5000);
-        }
+  async ApiJoinPlanet(planetId) {
+    const response = await this.RequestAPI(
+      'ITerritoryControlMinigameService/JoinPlanet',
+      [`access_token=${this.token}`, `id=${planetId}`],
+      { method: 'POST' },
+    );
+    return response;
+  }
+
+  async setupGame() {
+    debug(await this.ApiGetPlanets());
+
+    // while we haven't got a current planet
+      // TODO try follow preferences of the user (ie; planets with appid they want or specific name??)
+      // get first avaliable planet
+
+    // while the current planet is not the same as the steam planet
+      // leave the current game
+      // join the current planet
+      // get the planet steam thinks we joined
+  }
+
+  async gameLoop() {
+    // if last restart was greater than an hour ago
+      // change planet
+
+    // leave the current game to avoid getting stuck
+
+    // while !zone
+      // join first avaliable zone
+
+    // if there are no zones
+      // add planet to skip list
+      // restart
+
+    // if there are no hard zones left
+      // restart
+
+    // try to join a zone
+      // restart if failed
+
+    // log current zone info
+      // planet
+      // zone
+      // top clans (if exists)
+
+    // sleep for 110 seconds
+
+    // send reportScore
+
+    // if we get a new_score
+      // log current user stats
+  }
+
+  async init() {
+    try {
+      logger(chalk.bgGreen(` Started SalienScript | Version: ${pkgVersion} `));
+
+      await this.setupGame();
+
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        await this.gameLoop();
       }
+    } catch (e) {
+      logger(chalk.bgRed(`${e.name}:`), chalk.red(e.message));
+      logger(chalk.bgMagenta('Script will now restart...\n\n'));
 
-      while (!joinedZone) {
-        console.log('Attempting to join zone:', zone['zone_position']);
-
-        joinedZone = await joinZone(this.token, zone['zone_position']);
-
-        if (!joinedZone) {
-          console.log('Trying to get another Zone Position in 15 seconds...');
-
-          await delay(15000);
-        }
-      }
-
-      console.log(`Joined zone ${zone['zone_position']} - Captured: ${(zone['capture_progress'] * 100).toFixed(2)}% - Difficulty ${zone.difficulty}`);
-
-      console.log('Waiting 120 seconds for game to end...');
-
-      await delay(120000);
-
-      console.log('Game complete!');
-
-      await reportScore(this.token, getScoreForZone(zone));
+      this.init();
     }
-  };
+  }
 }
 
 module.exports = SalienScript;
