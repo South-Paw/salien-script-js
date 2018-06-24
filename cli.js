d1eca63445a6fff287f30673d0b762cf7f7228a6
Alex Gabites <alex@netspeed.net.nz>
2018-06-24 15:05:45 +1200

    oops

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const meow = require('meow');
-const SalienScript = require('../src/index.js');
+const SalienScript = require('./src/index.js');
 
 /** @type {meow.Options} */
 const cliOptions = {
