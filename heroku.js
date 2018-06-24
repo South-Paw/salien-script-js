8d1c55c460381d03a5089d30f4cf430eba889e98
hdk5 <humandk55@gmail.com>
2018-06-24 15:12:11 +0300

    Specify dynos

--- heroku.js
+++ heroku.js
@@ -2,12 +2,12 @@ const SalienScript = require('./src/index.js');
 
 const SALIEN_CONFIG = process.env.SALIEN_CONFIG;
 
-for (let config_str of SALIEN_CONFIG.split(";")) {
-    [token, clan, name] = config_str.split(":");
+for (let config_str of SALIEN_CONFIG.split(';')) {
+    [token, clan, name] = config_str.split(':');
     const config = {
         token: token,
         clan: clan,
-        name: name
+        name: name,
     };
 
     const salien = new SalienScript(config);
