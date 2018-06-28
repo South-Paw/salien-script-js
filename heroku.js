b8c16311fd0546f4f2435f902c9be4ec2d3b5600
nelsonlee2001 <nelsonlee2001@gmail.com>
2018-06-28 21:57:00 +0800

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -12,10 +12,16 @@ if (process.env.SALIEN_CONFIG_V2) {
     const [token, clan, name] = config.split(':');
 
     configs.push({
-      token,
-      clan,
-      name,
+        "token": "da79c2b97f4068ee3b87d5f8691df2a4",
+        "clan": "33035916",
+        "name": "first_acc"
+    },
+    {
+        "token": "a5636843a7056a12205a26d4b98d121f",
+        "clan": "33035916",
+        "name": "second_acc"
     });
+
   });
 }
 
