77a255775f3f45b410974fabc9eb2f718f608e75
Aaron-P <Aaron-P@users.noreply.github.com>
2018-06-30 19:42:16 -0500

    Fix linting

--- src/index.js
+++ src/index.js
@@ -222,8 +222,7 @@ class SalienScript {
       const report = await this.apiReportBossDamage(useHeal, damageToBoss, damageTaken);
 
       // eslint-disable-next-line no-underscore-dangle
-      if (Number(report.___headers.get('x-eresult')) !== 1 &&
-          Number(report.___headers.get('x-eresult')) !== 93) {
+      if (Number(report.___headers.get('x-eresult')) !== 1 && Number(report.___headers.get('x-eresult')) !== 93) {
         allowedBossFails -= 1;
 
         if (allowedBossFails < 1) {
